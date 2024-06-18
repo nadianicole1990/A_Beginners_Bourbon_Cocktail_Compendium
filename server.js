51fa1c90137cca178c6e7802f999445472736ade
Nadia Gushin <nadiagushin@Nadias-MacBook-Pro.local>
2024-06-18 18:50:05 -0400

    Set up basic express server && Pseudocode layout for server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// DEPENDENCIES && GLOBAL VARIABLES
+const express = require('express');
+const app = express();
+const port = 2222;
+
+// MIDDLEWARE
+
+// INDEX
+
+// NEW
+
+// DELETE
+
+// UPDATE
+
+// CREATE
+
+// EDIT
+
+// SHOW
+
+// LISTENERS
+app.listen(port, () => {
+    console.log(`I hear you loud and clear on port ${port}`);
+})
